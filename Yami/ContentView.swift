2ecf0fe7abcf986d8bccbe714d260f6ed449b6ec
jiajinda <jiajinda@yeah.net>
2023-03-30 00:36:26 -0400

    Initial Commit

---
+++ Yami/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Yami
+//
+//  Created by 贾金达 on 3/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yami/YamiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YamiApp.swift
+//  Yami
+//
+//  Created by 贾金达 on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct YamiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
